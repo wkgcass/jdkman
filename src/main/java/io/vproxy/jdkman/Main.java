7ca7ae5934ed9131ff1051387182ecdeac0612b1
wkgcass <wkgcass@hotmail.com>
2024-05-02 18:38:58 +0800

    1.0.3 release

--- src/main/java/io/vproxy/jdkman/Main.java
+++ src/main/java/io/vproxy/jdkman/Main.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class Main {
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     private static final String HELP_STR = """
         Usage:
             jdkman [action] [options]
