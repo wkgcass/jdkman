9c9354d2a33b66f04eb542930458bd779ff464ce
wkgcass <wkgcass@hotmail.com>
2024-05-28 20:42:21 +0800

    add more windows related jdk/bin executables

--- src/main/java/io/vproxy/jdkman/Main.java
+++ src/main/java/io/vproxy/jdkman/Main.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class Main {
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
     private static final String HELP_STR = """
         Usage:
             jdkman [action] [options]
